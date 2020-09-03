296b6a82f9ed6b9ab1d59a13f6e7e58300e46427
will c <will.cra@outlook.com>
2020-09-02 21:56:06 -0400

    added services

--- beachProject/src/app/app.component.ts
+++ beachProject/src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MarineWeatherService } from './marine-weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'beachProject';
+export class AppComponent{
 }
