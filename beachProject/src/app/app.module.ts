e4e36b3feaf48a17ceb880af675cd685c1e560dc
tanita plaengprawat <tanitaplaeng@gmail.com>
2020-09-07 18:38:30 -0400

    mobile landing page

--- beachProject/src/app/app.module.ts
+++ beachProject/src/app/app.module.ts
@@ -5,10 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { TeamComponent } from './team/team.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    TeamComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
