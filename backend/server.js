9e85f9fbe7fcaeaea23beaa50837dffef5a37bad
will c <will.cra@outlook.com>
2020-09-10 19:29:40 -0400

    added backend proxy

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,11 @@ const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
-app.get('/', (req, res) => {
-    request('https://maps.googleapis.com/maps/api/place/textsearch/json?key=AIzaSyD9NpL2p8Gn4sunZylBwxbNkDt7DbIiROs&query=Beaches', { json: true }, (err, result, body) => {
+app.get('/beaches', (req, res) => {
+    console.log(req.query.key);
+    request(`https://maps.googleapis.com/maps/api/place/textsearch/json?key=${req.query.key}&query=beaches in ${req.query.city}`, { json: true }, (err, result, body) => {
         res.send(body);
     });
 });
 
-app.listen(3000, () => console.log('listening'));
+app.listen(process.env.PORT || 3000, () => console.log('server is running'));
