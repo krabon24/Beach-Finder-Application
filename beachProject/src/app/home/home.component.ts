05b141631dedd6e5ca1ad79e5f9b3d112688d2f2
will c <will.cra@outlook.com>
2020-09-10 20:11:38 -0400

    moved over search function to search component

--- beachProject/src/app/home/home.component.ts
+++ beachProject/src/app/home/home.component.ts
@@ -9,39 +9,27 @@ import { BeachListService } from '../beach-list.service';
 })
 export class HomeComponent implements OnInit {
 
-  WeatherParams: object = {
-    q: "Detroit",
-    key: "fc1601812744410bbfb14416200309",
-    days: 3
-  }
+  // WeatherParams: object = {
+  //   q: "Detroit",
+  //   key: "fc1601812744410bbfb14416200309",
+  //   days: 3
+  // }
 
-  MarineParams: object = {
-    key: "826ac9715aae4317b1204900200309",
-    q: "34.03,118.87",
-    format: "json",
-    tide: "yes"
-  }
+  // MarineParams: object = {
+  //   key: "826ac9715aae4317b1204900200309",
+  //   q: "34.03,118.87",
+  //   format: "json",
+  //   tide: "yes"
+  // }
 
   // BeachParms: object = {
   //   key: "AIzaSyDD7HwmzWqfAPVxNACmt4Of17BVsbL3gjE",
   //   city: FormData.city
   // }
 
-  constructor(private weather: WeatherService, private beaches: BeachListService) { }
+  constructor() { }
 
   ngOnInit(): void {
-  }
-
-  searchFormSubmitted(formData: any){
-    const BeachParms: any = {
-      key: "AIzaSyDD7HwmzWqfAPVxNACmt4Of17BVsbL3gjE",
-      city: formData.city
-    }
-
-    this.beaches.getBeaches(BeachParms).subscribe((data: any) =>{
-      const beachFilter = data.hits.filter(beach => beach.results.formatted_address.includes(formData.city));
-      console.log(beachFilter);
-    });
   }
 
 }
