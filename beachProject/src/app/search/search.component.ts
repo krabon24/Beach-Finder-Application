88691033b1542e3081a504be5700ee08219d8529
will c <will.cra@outlook.com>
2020-09-17 20:52:04 -0400

    cleaned up code

--- beachProject/src/app/search/search.component.ts
+++ beachProject/src/app/search/search.component.ts
@@ -9,21 +9,9 @@ import { BeachListService } from '../beach-list.service';
 })
 export class SearchComponent implements OnInit {
 
-
-  MarineParams: object = {
-    key: "826ac9715aae4317b1204900200309",
-    q: "34.03,118.87",
-    format: "json",
-    tide: "yes"
-  }
-
-  markers: any = [];
-
-
   constructor(private weather: WeatherService, private beaches: BeachListService) { }
 
   ngOnInit(): void {
-    this.beaches.markers = this.markers;
   }
 
   searchFormSubmitted(formData: any) {
@@ -32,27 +20,21 @@ export class SearchComponent implements OnInit {
       city: formData.city
     }
     this.beaches.getBeaches(BeachParms).subscribe((data: any) => {
-      console.log(data.results)
       this.beaches.beachName = data.results.filter(beach => beach.formatted_address.includes(formData.city));
-      // console.log(this.beaches.beachName);
-
+      let markers: any = [];
       this.beaches.beachName.forEach((beach) => {
         this.weather.getMarineWeather(`${beach.geometry.location.lat},${beach.geometry.location.lng}`).subscribe((res) => {
           beach.weatherInfo = res;
-          console.log(this.beaches.beachName);
 
           let marker: any = {};
-          console.log(beach.geometry.location.lat);
           marker.position = new google.maps.LatLng({
             lat: beach.geometry.location.lat,
             lng: beach.geometry.location.lng,
           });
-          // console.log(marker)
-          this.markers.push(marker);
+          markers.push(marker);
         });
       });
-      this.beaches.markers = this.markers;
-      console.log(this.markers);
+      this.beaches.markers = markers;
     });
   };
 
